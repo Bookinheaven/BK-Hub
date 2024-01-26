504727eb55c15bb3161fbd66a1e6ea8f4cf98d78
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2024-01-26 16:32:03 +0530

    Update 1.Read_write.py

--- Python/File Handling/1.Read_write.py
+++ Python/File Handling/1.Read_write.py
@@ -1,4 +1,4 @@
-with open("./file_w.txt", "r+") as file:
+with open("./file_w.txt", "w+") as file:
     file.write('First Line\nSecond Line\nThird Line')
 # with open("./file_w.txt", "r+") as file:
     file.seek(0)
