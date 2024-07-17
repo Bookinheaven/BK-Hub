caab9d148c4bda9d49b61a8516b4bde458bb3d6e
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2024-07-17 21:20:56 +0530

    Java

---
+++ Java/F1.java
@@ -0,0 +1,19 @@
+package Exp01;
+
+import java.util.Scanner;
+/*
+ * Write a java program to read the age of a candidate and determine whether he is eligible to cast his/her own vote.
+ */
+public class F1 {
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+		System.out.println("Enter your age: ");
+		int Age = Integer.parseInt(inputScanner.nextLine());
+		if (Age >= 18) {
+			System.out.println("Eligible to vote");
+		} else {
+			System.out.println("Not Eligible to vote");
+		}
+		inputScanner.close();
+	}
+}
---
+++ Java/F3.java
@@ -0,0 +1,20 @@
+package Exp01;
+/*
+ * Write a java program to find the following pattern:
+
+*
+* *
+* * *
+* * * *
+
+ */
+public class F3 {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 4; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
