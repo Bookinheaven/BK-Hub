3db964252004c9919678829b86c249df32fc47cf
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2024-07-29 21:38:36 +0530

    Ex02 4 and 5 exp

---
+++ College/Second Year/PPS/Sem-1_Java/Exp02/F4.java
@@ -0,0 +1,38 @@
+package Ex02;
+
+import java.util.Scanner;
+
+public class F4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[3][3];
+        int[][] transpose_arr = new int[3][3];
+        System.out.println("Enter the elements;");
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+            	System.out.print("("+(i+1)+", "+(j+1)+"): ");
+                arr[i][j] = sc.nextInt(); 
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+            	System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                transpose_arr[i][j] = arr[j][i]; 
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                System.out.print(transpose_arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
---
+++ College/Second Year/PPS/Sem-1_Java/Exp02/F5.java
@@ -0,0 +1,35 @@
+package Ex02;
+
+import java.util.Scanner;
+
+public class F5 {
+	 public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int arr1[],arr2[];
+
+        System.out.println("Enter array 1 size");
+        int arr1_size = sc.nextInt();
+        arr1 = new int[arr1_size];
+        System.out.println("Enter array elements");
+        for(int i=0; i<arr1.length; i++){
+            arr1[i]= sc.nextInt();
+        }
+
+        System.out.println("Enter array 2 size");
+        int arr2_size = sc.nextInt();
+        arr2 = new int[arr2_size];
+        System.out.println("Enter array elements");
+        for(int i=0; i<arr2.length; i++){
+            arr2[i]= sc.nextInt();
+        }
+        System.out.println("Common elements");
+         for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr2.length; j++){
+                if (arr1[i] == arr2[j]){
+                    System.out.print(arr1[i] + ", ");
+                }
+            }
+         }
+         sc.close();
+    }
+}
