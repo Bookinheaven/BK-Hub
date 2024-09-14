cf77af3a3624cdb827f7ec00f8bf149ee7cc2529
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2024-09-14 20:35:54 +0530

    Create _2_SumOfElements.java

---
+++ Java/Arrays/_2_SumOfElements.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class _2_SumOfElements {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner sn = new Scanner(System.in);
+        System.out.print("Enter some numbers with space: ");
+        int array[] = sn.nextLine().strip().split(" ");
+        
+        sn.close();
+    }    
+}
