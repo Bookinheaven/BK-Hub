e1ab88b3299fc9d42255c4c3cb0ee9c5c15aa1e8
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2025-01-12 12:43:48 +0530

    Create 1.Log.js

---
+++ JavaScript/Basics/1.Log.js
@@ -0,0 +1,9 @@
+const id = "123123"
+let $username = "Burn" 
+var pass_word = "bk124" // older browsers
+
+let final = id.concat(" " + $username)
+console.log(id)
+console.log($username)
+console.log(pass_word)
+console.log(final)
