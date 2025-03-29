2bec4063b86466d60f320f2b6b0988c42a22d988
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2025-03-29 14:50:57 +0530

    CTF 13

---
+++ Front-End/Other/Works/CTFs/13_Thor’s Rune Cipher/main.js
@@ -0,0 +1,17 @@
+function downloadHint() {
+    let link = document.createElement("a");
+    link.href = "hint.jpg"; 
+    link.download = "hint.jpg";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+console.log("Odin whispers: 'The power was never in the hammer, Thor...'");
+
+let asdas = [
+    "e01DX0NURjpZb3VBcmVBRm9vbH0=",
+    "7B4D435F4354463A4964696F747D",
+    "01111011,01001101,01000011,01011111,01000011,01010100,01000110,00111010,01000001,01110010,01100101,01000100,01110101,01101101,01100010,01000100,01101111,01011001,01101111,01110101,01010100,01101000,01101001,01101110,01101011,01001001,01010111,01101001,01101100,01101100,01001000,01101001,01100100,01100101,01001000,01100101,01110010,01100101,00111111,01111101"
+];
+console.log("SGludDogT2RpbiBoYXMgbGVmdCBhIG1lc3NhZ2UgaW5zaWRlIGFuIGFuY2llbnQgaW1hZ2UuIE9wZW4gJ2hpbnQuanBnJy4");
+console.log("Decrypting Asgardian data requires Base64 knowledge...");
