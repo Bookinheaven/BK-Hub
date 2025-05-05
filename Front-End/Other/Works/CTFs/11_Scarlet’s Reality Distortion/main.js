3316285efce3648c6d6ec2ee03ef87cd01113dce
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2025-05-05 21:54:42 +0530

    Separated CTF

--- Front-End/Other/Works/CTFs/11_Scarlet’s Reality Distortion/main.js
+++
@@ -1,8 +0,0 @@
-function downloadImage() {
-    const link = document.createElement('a');
-    link.href = 'wanda_reality_hidden.png';
-    link.download = 'wanda_reality_hidden.png';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
